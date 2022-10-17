3617c487033bc895f1921c6035e822c50759a607
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-10-17 13:31:09 +0200

    Modified

---
+++ src/data/data.js
@@ -0,0 +1,55 @@
+export const dataTeam = [
+    {
+        id: 1,
+        name: "Earl Grey",
+        email: "earl@testemail.com",
+        age: 29,
+        phone: "(00)234-2738",
+        access: "admin",
+    },
+
+    {
+        id: 2,
+        name: "Amy Earl",
+        email: "amy@testemail.com",
+        age: 26,
+        phone: "(00)398-6590",
+        access: "admin",
+    },
+
+    {
+        id: 3,
+        name: "Matthew Peters",
+        email: "matt@testemail.com",
+        age: 34,
+        phone: "(00)900-6711",
+        access: "manager",
+    },
+
+    {
+        id: 4,
+        name: "Joelle Smith",
+        email: "joelle@testemail.com",
+        age: 30,
+        phone: "(00)760-0118",
+        access: "user",
+    },
+
+    {
+        id: 5,
+        name: "Paula Venter",
+        email: "paula@testemail.com",
+        age: 27,
+        phone: "(00)234-2738",
+        access: "manager",
+    },
+
+    {
+        id: 6,
+        name: "Steve Gant",
+        email: "steve@testemail.com",
+        age: 32,
+        phone: "(00)208-7820",
+        access: "admin",
+    },
+]
