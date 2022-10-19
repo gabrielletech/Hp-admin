239764766734d9efa6deb61789467456ff452813
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-10-19 16:04:54 +0200

    Modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import Topbar from './scenes/global/Topbar'
-import Sidebar from '.scenes/global/Sidebar'
-// import Dashboard from '.scenes/dashboard'
+import Topbar from './scenes/global/Topbar';
+import SideBar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard'
 // import Team from './scenes/team'
 // import Invoices from './scenes/invoices'
 // import Contacts from './scenes/contacts'
@@ -13,33 +13,36 @@ import Sidebar from '.scenes/global/Sidebar'
 // import FAQ from './scenes/faq'
 // import Geography from './scenes/geography'
 // import Calendar from './scenes/calendar'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
+import { ProSidebarProvider } from 'react-pro-sidebar';
 
 function App() {
   const [theme, colorMode] = useMode();
   return (
   <ColorModeContext.Provider value={colorMode}>
     <ThemeProvider theme={theme}>
+      <ProSidebarProvider>
         <CssBaseline />
         <div className="app">
-          <Sidebar />
+          <SideBar />
           <main className="content">
             <Topbar />
             <Routes>
-                {/* <Route path='/' element={<Dashboard />} /> */}
-                {/* <Route path='/team' element={<Team />} /> */}
-                {/* <Route path='/invoices' element={<Invoices />} /> */}
-                {/* <Route path='/contacts' element={<Contacts />} /> */}
-                {/* <Route path='/bar' element={<Bar />} /> */}
-                {/* <Route path='/form' element={<Form />} /> */}
-                {/* <Route path='/pie' element={<Pie />} /> */}
-                {/* <Route path='/line' element={<Line />} /> */}
-                {/* <Route path='/faq' element={<FAQ />} /> */}
-                {/* <Route path='/geography' element={<Geography />} /> */}
-                {/* <Route path='/calendar' element={<Calendar />} /> */}
+                 <Route path='/' element={<Dashboard />} /> 
+                {/* <Route path='/team' element={<Team />} /> 
+                 <Route path='/invoices' element={<Invoices />} />
+                 <Route path='/contacts' element={<Contacts />} />
+                 <Route path='/bar' element={<Bar />} />
+                 <Route path='/form' element={<Form />} />
+                 <Route path='/pie' element={<Pie />} />
+                 <Route path='/line' element={<Line />} />
+                 <Route path='/faq' element={<FAQ />} />
+                 <Route path='/geography' element={<Geography /> } />
+                 <Route path='/calendar' element={<Calendar />} /> */}
             </Routes>
           </main>
         </div>
+    </ProSidebarProvider>
     </ThemeProvider>
   </ColorModeContext.Provider>
     
