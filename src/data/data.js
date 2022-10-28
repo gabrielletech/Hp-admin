eedfc3a3f5a9c8c55f1200068a97714fc7552baa
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-10-28 16:09:18 +0200

    created Contacts and Invoices Scenes

--- src/data/data.js
+++ src/data/data.js
@@ -51,5 +51,186 @@ export const dataTeam = [
         age: 32,
         phone: "(00)208-7820",
         access: "admin",
+    }
+];
+
+export const dataContacts = [
+    {
+        id: 1,
+        name: "Eliza Charemba",
+        email: "eliza@testemail.com",
+        age: 30,
+        phone: "(081) 763-1237",
+        address: "123 Nehanda Street, Harare CBD",
+        city: "Harare",
+        zipCode: "0000",
+        registrarId: 134907
     },
-]
+    {
+        id: 2,
+        name: "Takudzwa Vengai",
+        email: "takudzwa@testemail.com",
+        age: 40,
+        phone: "(081) 340-1290",
+        address: "14 Drew Road, Chisipite",
+        city: "Harare",
+        zipCode: "0000",
+        registrarId: 165098
+    },
+    {
+        id: 3,
+        name: "Michelle Kamwendo",
+        email: "michelle@testemail.com",
+        age: 25,
+        phone: "(081) 094-2300",
+        address: "18 Aintree, Highlands",
+        city: "Harare",
+        zipCode: "0000",
+        registrarId: 197543
+    },
+    {
+        id: 4,
+        name: "Tracy Hilton",
+        email: "tracy@testemail.com",
+        age: 34,
+        phone: "(081) 900-3427",
+        address: "51 Kensington Rd, Mt Pleasant",
+        city: "Harare",
+        zipCode: "0000",
+        registrarId: 187329
+    },
+    {
+        id: 5,
+        name: "Garvin Vambe",
+        email: "garvin@testemail.com",
+        age: 38,
+        phone: "(081) 888-3400",
+        address: "64 Suffolk Rd, Emerald Hill",
+        city: "Harare",
+        zipCode: "0000",
+        registrarId: 198802
+    },
+    {
+        id: 6,
+        name: "Rukudzo Revai",
+        email: "rukudzo@testemail.com",
+        age: 34,
+        phone: "(081) 877-2300",
+        address: "233 Samora Machel Avenue",
+        city: "Harare",
+        zipCode: "0000",
+        registrarId: 100927
+    },
+    {
+        id: 7,
+        name: "Amy Kadungure",
+        email: "amy@testemail.com",
+        age: 27,
+        phone: "(081) 890-0911",
+        address: "1644 Broadacres Avenue",
+        city: "Harare",
+        zipCode: "0000",
+        registrarId: 120094
+    },
+    {
+        id: 8,
+        name: "Joseph Tongogara",
+        email: "joseph@testemail.com",
+        age: 48,
+        phone: "(081) 981-1114",
+        address: "16 Old Enterprise Rd, Newlands",
+        city: "Harare",
+        zipCode: "0000",
+        registrarId: 103334
+    },
+    {
+        id: 9,
+        name: "Fadzai Muusha",
+        email: "fadzai@testemail.com",
+        age: 25,
+        phone: "(081) 099-2344",
+        address: "26 Coronation Rd, Greendale",
+        city: "Harare",
+        zipCode: "0000",
+        registrarId: 122091
+    },
+    {
+        id: 10,
+        name: "Felix Pasipanodya",
+        email: "felix@testemail.com",
+        age: 39,
+        phone: "(081) 223-0267",
+        address: "20 Arcturus Rd",
+        city: "Harare",
+        zipCode: "0000",
+        registrarId: 187774
+    },
+    
+];
+
+export const dataInvoices = [
+  {
+    id: 1,
+    name: "Jon Snow",
+    email: "jonsnow@gmail.com",
+    cost: "21.24",
+    phone: "(665)121-5454",
+    date: "03/12/2022",
+  },
+  {
+    id: 2,
+    name: "Cersei Lannister",
+    email: "cerseilannister@gmail.com",
+    cost: "1.24",
+    phone: "(421)314-2288",
+    date: "06/15/2021",
+  },
+  {
+    id: 3,
+    name: "Jaime Lannister",
+    email: "jaimelannister@gmail.com",
+    cost: "11.24",
+    phone: "(422)982-6739",
+    date: "05/02/2022",
+  },
+  {
+    id: 4,
+    name: "Anya Stark",
+    email: "anyastark@gmail.com",
+    cost: "80.55",
+    phone: "(921)425-6742",
+    date: "03/21/2022",
+  },
+  {
+    id: 5,
+    name: "Daenerys Targaryen",
+    email: "daenerystargaryen@gmail.com",
+    cost: "1.24",
+    phone: "(421)445-1189",
+    date: "01/12/2021",
+  },
+  {
+    id: 6,
+    name: "Ever Melisandre",
+    email: "evermelisandre@gmail.com",
+    cost: "63.12",
+    phone: "(232)545-6483",
+    date: "11/02/2022",
+  },
+  {
+    id: 7,
+    name: "Ferrara Clifford",
+    email: "ferraraclifford@gmail.com",
+    cost: "52.42",
+    phone: "(543)124-0123",
+    date: "02/11/2022",
+  },
+  {
+    id: 8,
+    name: "Rossini Frances",
+    email: "rossinifrances@gmail.com",
+    cost: "21.24",
+    phone: "(222)444-5555",
+    date: "05/02/2021",
+  },
+];
